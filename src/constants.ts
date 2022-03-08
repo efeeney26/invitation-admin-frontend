50d73e45a92318308cad7b369006c75295b281b1
Kochkin Danila <>
2022-03-08 14:20:36 +0300

    resource

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_INVITATION = 'Это стандартное приглашение. Наверное, оно будет длинным';
 
-export const RESOURCE = 'Какой-то ресурс';
+export const RESOURCE = 'https://efeeney26.github.io/invitaion-frontend/';
